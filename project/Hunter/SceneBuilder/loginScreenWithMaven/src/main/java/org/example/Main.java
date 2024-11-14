50f101b04ad5f9ee0afd96750a9a6f434a0fe1b6
hunterAntal <hunterantalgithub@gmail.com>
2024-11-14 12:43:13 -0500

    trying to get scenebuilder to work with intellij

--- project/Hunter/SceneBuilder/loginScreenWithMaven/src/main/java/org/example/Main.java
+++ project/Hunter/SceneBuilder/loginScreenWithMaven/src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
