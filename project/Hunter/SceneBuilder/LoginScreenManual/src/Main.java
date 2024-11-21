5108cc0c92a8d5370db386e4b6d9696ce64bda90
hunterAntal <hunterantalgithub@gmail.com>
2024-11-21 12:33:20 -0500

    Got the thing to load the fxml file

---
+++ project/Hunter/SceneBuilder/LoginScreenManual/src/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
+        Parent root = FXMLLoader.load(getClass().getResource("loginScreen2.fxml"));
+        primaryStage.setTitle("JavaFX Without Maven");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
