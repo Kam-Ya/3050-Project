a7675833e12ec6cb21282653a29409eda2c01dfb
Jamie Grasley <16744724+JamieGrasley@users.noreply.github.com>
2024-12-01 16:15:35 -0500

    Uploading OCSF serv files

---
+++ server/Server/src/format/msgFormat.java
@@ -0,0 +1,20 @@
+package com.format;
+import java.io.*;
+//msgFormat implements serializable
+//Provides a standard way to exchange objects between the client and the server
+public class msgFormat implements Serializable{
+    //This dictates what type of operation is needed based on standardized message strings
+    public String type;
+
+    //This is the object that will be exchanged
+    public Object obj;
+
+    //This passes the userID for any necessary permissions checking
+    public Integer genericId;
+
+    public msgFormat(String type, Object obj, Integer genericId){
+        this.type=type;
+        this.obj=obj;
+        this.genericId=genericId;
+    }
+}
